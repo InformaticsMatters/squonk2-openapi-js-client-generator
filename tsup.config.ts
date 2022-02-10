64fdb59aa3b4924535b2b1165a0cd53b4f2593c5
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-02-10 15:48:10 +0000

    chore(tsup): enable splitting (for custom instance sharing?)

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,9 @@ import type { Options } from 'tsup';
 export const tsup: Options = {
   dts: true,
   minify: false,
-  splitting: false,
-  sourcemap: 'external',
-  clean: true,
+  splitting: true,
+  sourcemap: true,
+  clean: false,
   format: ['esm', 'cjs'],
   entryPoints: ['src/index.ts', 'src/*/*.ts'],
   target: 'node16',
