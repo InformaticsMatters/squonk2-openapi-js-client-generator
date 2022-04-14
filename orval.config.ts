7c804761f1fef9800c5e74d175862c24e1a85763
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-04-14 15:51:36 +0100

    fix(orval): Remove old config file and transformer line

--- orval.config.ts
+++ orval.config.ts
@@ -5,21 +5,18 @@ export default defineConfig({
   ORVAL_API_NAME: {
     input: {
       target: './openapi.yaml',
-      override: {
-        transformer: './input-transformer.cjs',
-      },
     },
     output: {
       mode: 'tags-split',
       // API_TARGET_NAME below is replaced in the action
       target: './src/API_TARGET_NAME.ts',
       prettier: true,
-      client: 'react-query',
+      client: 'axios',
       override: {
-        mutator: {
-          path: './src/custom-instance.ts',
-          name: 'customInstance',
-        },
+        // mutator: {
+        //   path: './src/custom-instance.ts',
+        //   name: 'customInstance',
+        // },
         query: {
           useQuery: true,
         },
